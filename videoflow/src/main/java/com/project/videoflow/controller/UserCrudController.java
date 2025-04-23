f9008dfe224b4c1320ca113b6ae6850c3512de82
Fekete Anna <annafekete85@gmail.com>
2025-04-23 15:06:28 +0200

    .

--- videoflow/src/main/java/com/project/videoflow/controller/UserCrudController.java
+++ videoflow/src/main/java/com/project/videoflow/controller/UserCrudController.java
@@ -1,6 +1,8 @@
 package com.project.videoflow.controller;
 
+import com.project.videoflow.model.Role;
 import com.project.videoflow.model.User;
+import com.project.videoflow.repository.RoleRepository;
 import com.project.videoflow.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,44 +10,51 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/admin/users")
+@RequestMapping("/crud")
 public class UserCrudController {
 
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private RoleRepository roleRepository;
+
     @GetMapping
     public String listUsers(Model model) {
         model.addAttribute("users", userRepository.findAll());
         model.addAttribute("newUser", new User());
-        return "userCrud";
+        model.addAttribute("roles", roleRepository.findAll());
+        return "user-crud";
     }
 
     @PostMapping("/add")
     public String addUser(@ModelAttribute("newUser") User user) {
         userRepository.save(user);
-        return "redirect:/admin/users";
+        return "redirect:/crud";
+    }
+
+    @GetMapping("/delete/{email}")
+    public String deleteUser(@PathVariable String email) {
+        userRepository.deleteById(email);
+        return "redirect:/crud";
     }
 
     @PostMapping("/edit/{email}")
     public String editUser(@PathVariable String email,
                            @RequestParam("felhasznalonev") String felhasznalonev,
-                           @RequestParam("jelszo") String jelszo) {
-        User existingUser = userRepository.findByEmail(email);
-        if (existingUser != null) {
-            existingUser.setFelhasznalonev(felhasznalonev);
-            existingUser.setJelszo(jelszo);
-            userRepository.save(existingUser);
-        }
-        return "redirect:/admin/users";
-    }
+                           @RequestParam("jelszo") String jelszo,
+                           @RequestParam("szerepkorId") Long szerepkorId) {
 
-    @GetMapping("/delete/{email}")
-    public String deleteUser(@PathVariable String email) {
-        User user = userRepository.findByEmail(email);
-        if (user != null) {
-            userRepository.delete(user);
+        User user = userRepository.findById(email).orElse(null);
+        Role role = roleRepository.findById(szerepkorId).orElse(null);
+
+        if (user != null && role != null) {
+            user.setFelhasznalonev(felhasznalonev);
+            user.setJelszo(jelszo);
+            user.setSzerepkor(role);
+            userRepository.save(user);
         }
-        return "redirect:/admin/users";
+
+        return "redirect:/crud";
     }
 }
